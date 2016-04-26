bffc450dcc44ce36b3a6a3de42e1d5c3576ec7a5
Forbzz <ta@et.esiea.fr>
2016-04-26 17:36:59 -0400

    first commit

---
+++ app/src/main/java/adapters/DeveloperAdapter.java
@@ -0,0 +1,38 @@
+package adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+
+import java.util.List;
+
+import models.DeveloperModel;
+
+/**
+ * Created by Nicolas on 26/04/2016.
+ */
+public class DeveloperAdapter extends CardScrollAdapter {
+    private List<CardBuilder> mCards;
+    private List<DeveloperModel> mData;
+    public DeveloperAdapter(List<CardBuilder> cards){
+        this.mCards = cards;
+    }
+    @Override
+    public int getCount() {
+        return mCards.size();
+    }
+    @Override
+    public Object getItem(int i) {
+        return mCards.get(i);
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return mCards.get(i).getView();
+    }
+    @Override
+    public int getPosition(Object o) {
+        return this.mCards.indexOf(o);
+    }
+}
